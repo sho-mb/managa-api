66050bf72d1337adf2fa36243c5107ac1c877bb6
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-07-08 13:05:38 +0700

    feat: add logic update

--- src/manga/services/manga.service.ts
+++ src/manga/services/manga.service.ts
@@ -1,7 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { Manga } from '../entity/manga.entity';
 import { MangaRepository } from '../manga.repository';
 import { CreateMangaDto } from '../requests/create-manga.dto';
+import { UpdateMangaDto } from '../requests/update-mange.dto';
 
 @Injectable()
 export class MangaService {
@@ -19,6 +20,17 @@ export class MangaService {
 
   async create(manga: CreateMangaDto): Promise<Manga> {
     return this.mangaRepository.createNewManga(manga);
+  }
+
+  async update(id: string, updateMangaDto: UpdateMangaDto): Promise<Manga> {
+    const manga = await this.mangaRepository.findOneById(id);
+    if (!manga) {
+      throw new HttpException('Item not exist', HttpStatus.BAD_REQUEST);
+    }
+
+    Object.assign(manga, updateMangaDto);
+
+    return this.mangaRepository.updateManga(manga);
   }
 
   async deleteOne(id: string): Promise<void> {
